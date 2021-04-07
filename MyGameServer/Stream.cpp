23d2d7a7148fe14b56704a9e1a0815af0702e702
KimDaeEon <eo956578rr@gmail.com>
2021-04-07 23:29:41 +0900

    CStream Read Functions added

--- MyGameServer/Stream.cpp
+++ MyGameServer/Stream.cpp
@@ -24,7 +24,7 @@ BOOL CStream::SetBuffer(BYTE* buffer) {
 }
 
 /// <summary>
-/// INT32형 데이터를 읽어올 때에 사용합니다.
+/// INT32 형 데이터를 읽어올 때에 사용합니다.
 /// </summary>
 /// <param name="data"></param>
 /// <returns></returns>
@@ -32,20 +32,108 @@ BOOL CStream::ReadInt32(INT* data) {
 	// CopyMemory 를 이용해서 지정된 버퍼에 값을 복사합니다.
 	CopyMemory(data, mBufferPointer + mLength, sizeof(INT));
 
-	// 스트림 길이를 데이터의 길이만큼 더해줍니다.
+	// 스트림 길이를 데이터의 길이만큼 더해줍니다. 앞으로 나오는 Read 함수들은 이런 형식으로 작성됩니다.
 	mLength += sizeof(INT);
 
 	return TRUE;
 }
 
 /// <summary>
-/// DWORD형 데이터를 읽을 때 사용합니다.
+/// INT64 형 데이터를 읽을 때 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+BOOL CStream::ReadInt64(INT64* data) {
+	CopyMemory(data, mBufferPointer + mLength, sizeof(INT64));
+	mLength += sizeof(INT64);
+	return TRUE;
+}
+
+/// <summary>
+/// DWORD 형 데이터를 읽을 때 사용합니다.
 /// </summary>
 /// <param name="data"></param>
 /// <returns></returns>
 BOOL CStream::ReadDWORD(DWORD* data) {
 	CopyMemory(data, mBufferPointer + mLength, sizeof(DWORD));
+	mLength += sizeof(DWORD);
+	return TRUE;
+}
+
+/// <summary>
+/// DWORD_PTR 형 데이터를 읽을 때 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+BOOL CStream::ReadDWORD_PTR(DWORD_PTR* data) {
+	CopyMemory(data, mBufferPointer + mLength, sizeof(DWORD_PTR));
 	mLength += sizeof(DWORD_PTR);
 	return TRUE;
 }
 
+/// <summary>
+/// BYTE 형 데이터를 읽을 때 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+BOOL CStream::ReadByte(BYTE* data) {
+	CopyMemory(data, mBufferPointer + mLength, sizeof(BYTE));
+	mLength += sizeof(BYTE);
+	return TRUE;
+}
+
+/// <summary>
+/// BYTE 형 데이터를 length 만큼 읽어올 때에 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <param name="length"></param>
+/// <returns></returns>
+BOOL CStream::ReadBytes(BYTE* data, DWORD length) {
+	CopyMemory(data, mBufferPointer + mLength, length);
+	mLength += length;
+	return TRUE;
+}
+
+/// <summary>
+/// FLOAT 형 데이터를 읽을 때 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+BOOL CStream::ReadFloat(FLOAT* data) {
+	CopyMemory(data, mBufferPointer + mLength, sizeof(FLOAT));
+	mLength += sizeof(FLOAT);
+	return TRUE;
+}
+
+/// <summary>
+/// SHORT 형 데이터를 읽을 때 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+BOOL CStream::ReadSHORT(SHORT* data) {
+	CopyMemory(data, mBufferPointer + mLength, sizeof(SHORT));
+	mLength += sizeof(SHORT);
+	return TRUE;
+}
+
+/// <summary>
+/// USHORT 형 데이터를 읽을 때 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+BOOL CStream::ReadUSHORT(USHORT* data) {
+	CopyMemory(data, mBufferPointer + mLength, sizeof(USHORT));
+	mLength += sizeof(USHORT);
+	return TRUE;
+}
+
+/// <summary>
+/// BOOL 형 데이터를 읽을 때 사용합니다.
+/// </summary>
+/// <param name="data"></param>
+/// <returns></returns>
+BOOL CStream::ReadBOOL(BOOL* data) {
+	CopyMemory(data, mBufferPointer + mLength, sizeof(BOOL));
+	mLength += sizeof(BOOL);
+	return TRUE;
+}
--- MyGameServer/Stream.h
+++ MyGameServer/Stream.h
@@ -12,15 +12,29 @@ private:
 public:
 	BOOL SetBuffer(BYTE* buffer);
 
+	// Read 함수
 	BOOL ReadInt32(INT* data);
+	BOOL ReadInt64(INT64* data);
 	BOOL ReadDWORD(DWORD* data);
 	BOOL ReadDWORD_PTR(DWORD_PTR* data);
 	BOOL ReadByte(BYTE* data);
 	BOOL ReadBytes(BYTE* data, DWORD length);
 	BOOL ReadFloat(FLOAT* data);
-	BOOL ReadInt64(INT64* data);
-	BOOL ReadUSHORT(USHORT* data); // TODO: 함수명이 왜 USHORT 형태일까
-	BOOL ReadBOOL(BOOL data); // TODO: 함수명이 왜 BOOL 형태일까
+	BOOL ReadSHORT(SHORT* data); 
+	BOOL ReadUSHORT(USHORT* data); 
+	BOOL ReadBOOL(BOOL* data); 
+
+	// Write 함수
+	BOOL WriteInt32(INT data);
+	BOOL WriteInt64(INT64 data);
+	BOOL WriteDWORD(DWORD data);
+	BOOL WriteDWORD_PTR(DWORD_PTR data);
+	BOOL WriteByte(BYTE data);
+	BOOL WriteBytes(BYTE* data, DWORD length);
+	BOOL WriteFloat(FLOAT data);
+	BOOL WriteSHORT(SHORT data);
+	BOOL WriteUSHORT(USHORT data);
+	BOOL WriteBOOL(BOOL data);
 
 	DWORD GetLength(VOID);
 };
