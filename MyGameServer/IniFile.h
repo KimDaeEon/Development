c1e99828606b5a02ffd29ab1af5dd1b866c23fe1
KimDaeEon HOME <eo956578rr@gmail.com>
2021-05-10 00:38:10 +0900

    Add 'IniFile.h' and 'IniFile.cpp'.

---
+++ MyGameServer/IniFile.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Windows.h>
+#include <tchar.h>
+
+class CIniFile {
+public:
+	CIniFile(VOID);
+	virtual ~CIniFile(VOID);
+
+private:
+	TCHAR mFileName[MAX_PATH];
+
+public:
+	BOOL Open(LPCTSTR fileName);
+	BOOL Close(VOID);
+
+	BOOL SetValue(LPCTSTR keyName, LPCTSTR valueName, LPCTSTR value);
+	BOOL SetValue(LPCTSTR keyName, LPCTSTR valueName, DWORD value);
+	BOOL SetValue(LPCTSTR keyName, LPCTSTR valueName, FLOAT value);
+
+	BOOL GetValue(LPCTSTR keyName, LPCTSTR valueName, LPTSTR value, LPDWORD bufferLength);
+	BOOL GetValue(LPCTSTR keyName, LPCTSTR valueName, LPDWORD value);
+	BOOL GetValue(LPCTSTR keyName, LPCTSTR valueName, FLOAT* value);
+};
