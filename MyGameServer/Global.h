5d4e27852bd2b12712347bc424e25f5a206c74bc
KimDaeEon HOME <eo956578rr@gmail.com>
2021-06-13 03:04:30 +0900

    add tcp socket practice code.

--- MyGameServer/Global.h
+++ MyGameServer/Global.h
@@ -1,6 +1,6 @@
 #pragma once
 #include<WinSock2.h>
-#include<Windows.h> // <- 여기에 winsock.h 이 포함되어 있다. 이로 인해 합수가 겹치는 것 막으려면 WinSock2.h 를 먼저 include 하라고 쓰여 있다.
+#include<Windows.h> // TODO: <- 여기에 winsock.h 이 포함되어 있다. 이로 인해 합수가 겹치는 것 막으려면 WinSock2.h 를 먼저 include 하라고 쓰여 있다. 맞는지 실험해보자.
 #include<tchar.h>
 #include<fstream>
 #include<pchannel.h>
