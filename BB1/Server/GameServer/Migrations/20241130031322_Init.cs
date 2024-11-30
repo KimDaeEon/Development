3cca89120007a452afdacc4b45e5205bd947961a
64GB-I7-DAEEON\eo956 <eo956578rr@gmail.com>
2024-11-30 15:48:59 +0900

    1. delete previous client project. 2. commit new client project.

---
+++ BB1/Server/GameServer/Migrations/20241130031322_Init.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Character1",
+                table: "Character1");
+
+            migrationBuilder.RenameTable(
+                name: "Character1",
+                newName: "Character");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Character1_AccountDbId",
+                table: "Character",
+                newName: "IX_Character_AccountDbId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Character",
+                table: "Character",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Character",
+                table: "Character");
+
+            migrationBuilder.RenameTable(
+                name: "Character",
+                newName: "Character1");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Character_AccountDbId",
+                table: "Character1",
+                newName: "IX_Character1_AccountDbId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Character1",
+                table: "Character1",
+                column: "Id");
+        }
+    }
+}
