f6d0f13cbc8a0a57868ebd998140fd49103c7aa1
64GB-I7-DAEEON\eo956 <eo956578rr@gmail.com>
2025-03-19 15:43:03 +0900

    1. add git ignore. 2. add study materials.

---
+++ Study/CStudyAndCodingTest/Dll1/dllmain.cpp
@@ -0,0 +1,13 @@
+﻿#include <windows.h>
+#include <iostream>
+
+extern "C" __declspec(dllexport) void HelloFunction()
+{
+    std::cout << "Hello from DLL!" << std::endl;
+}
+
+int main()
+{
+    HelloFunction(); // 정적으로 링크된 함수 호출
+    return 0;
+}
